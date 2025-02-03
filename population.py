baa5b0c73e23ebfe8a7f45f7a3fe673bb24551c9
ernes <ernestopatrono@gmail.com>
2025-02-03 21:39:08 +0100

    just started, checking how to draw the grid

--- population.py
+++ population.py
@@ -3,5 +3,17 @@
 Population project
 """
 
-x=3
+#Aca voy a intentar
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+GRID_SIZE = (1000, 900)
+
+grid = np.random.rand(*GRID_SIZE)
+
+plt.imshow(grid, cmap='viridis', interpolation='nearest')
+plt.colorbar()  # Adds a color bar to the side to show the value-to-color mapping
+plt.title("Grid Visualization with Color Tones")
+plt.show()
